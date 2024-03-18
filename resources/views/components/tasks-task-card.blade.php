584a4c02c204c16b21add378db2659b5a43f7ee0
Moisés <hyrared@gmail.com>
2024-03-18 18:14:09 +0100

    feat(UI): Update tasks list

--- resources/views/components/tasks-task-card.blade.php
+++ resources/views/components/tasks-task-card.blade.php
@@ -8,24 +8,39 @@
 
                 <p class="mb-2">{{ $task->description }}</p>
 
-                <p>Started on: {{ $task->startTime->label }}</p>
-                <p>Time limit: <strong class="text-red-500">{{ $task->expired_at }}</strong></p>
+                <p>Started time: <strong class="text-gray-500">{{ $task->startTime->label }}</strong></p>
+                <p>Time limit to complete:
+                    <strong class="text-gray-500">{{ $task->expirationTime ? $task->expirationTime->label : 'without time limit' }}</strong>
+                </p>
+
             </details>
         </div>
 
         <div class="min-w-180 text-right">
-            @if ($task->expired_at)
-                <strong class="text-green-600" data-tooltip-target="closed-at-tooltip--{{ $task->id }}">Ended</strong>
-                <div id="closed-at-tooltip--{{ $task->id }}" role="tooltip" class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
-                    {{ $task->ended_at }}
-                    <div class="tooltip-arrow" data-popper-arrow></div>
-                </div>
-            @else
-                {{-- <x-tasks-start-times-selector :startTimes="$startTimes" :taskId="$task->id"/> --}}
-            @endif
+            <span class="text-green-600" data-tooltip-target="active-period-tooltip--{{ $task->id }}">
+                @if ($task->start_at <= now()
+                    && now() <= $task->expired_at)
+                    <strong class="text-green-600">
+                        Active
+                    </strong>
+                @else
+                    <strong class="text-red-600">
+                        Inactive
+                    </strong>
+                @endif
+            </span>
+
+            <div id="active-period-tooltip--{{ $task->id }}"
+                role="tooltip"
+                class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip">
+                Active from: {{ $task->start_at }} to {{ $task->expired_at }}
+                <div class="tooltip-arrow" data-popper-arrow></div>
+            </div>
         </div>
     </div>
-    <p class="flex justify-between mt-5 md:mt-8 bg-gray-200 p-1 md:p-2 md:py-3"><span>Group:</span><strong> {{ $task->group->title }}</strong></p>
+    <p class="flex justify-between mt-5 md:mt-8 bg-gray-200 p-1 md:p-2 md:py-3">
+        <span>Group:</span><strong> {{ $task->group->title }}</strong>
+    </p>
 </article>
 
 @push('custom-scripts')
